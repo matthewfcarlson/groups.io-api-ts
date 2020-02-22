84471390f62c424dc8706ad1c02cac1e96d8887d
Matthew Carlson <matthewfcarlson@gmail.com>
2020-02-21 22:25:22 -0800

    Added new files

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export class Car {
+  public height: number = 0;
+  public width: number = 0;
+  public brand: string = '';
+  public model: string = '';
+
+  Run() {
+    // tslint:disable-next-line:no-console
+    console.log('HELLO');
+  }
+}
