595935cc65cf9bdeb1deaec092f6ba1121b46c88
Matthew Carlson <matthewfcarlson@gmail.com>
2020-02-21 22:55:44 -0800

    the start of the axios

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
-export class Car {
-  public height: number = 0;
-  public width: number = 0;
-  public brand: string = '';
-  public model: string = '';
+import axios from 'axios';
 
-  Run() {
-    // tslint:disable-next-line:no-console
-    console.log('HELLO');
+interface GroupsIOLoginApiKey {
+  api_key: string;
+}
+
+interface GroupsIOLoginUser {
+  email: string;
+  password: string;
+  twofactor?: number;
+}
+
+type GroupIOLogin = GroupsIOLoginApiKey | GroupsIOLoginUser;
+
+export class GroupsIO {
+  private apiKey = null;
+  private email = '';
+  private apiToken: GroupIOLogin = null;
+  readonly DEFAULT_HOSTNAME = 'api.groups.io';
+  readonly DEFAULT_SCHEME = 'https';
+  readonly DEFAULT_API_BASE = this.DEFAULT_SCHEME + '://' + this.DEFAULT_HOSTNAME;
+  readonly DEFAULT_VERSION = 'v1';
+  readonly DEFAULT_VERSIONED_API_BASE = this.DEFAULT_API_BASE + '/' + this.DEFAULT_VERSION + '/';
+
+  IsLoggedIn() {
+    return this.apiKey != null;
+  }
+
+  async Login(login: GroupIOLogin): Promise<boolean> {
+    // try to login with this authentication method
+    return false;
   }
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,4 +1,6 @@
-import { Car } from '../src/index';
-test('My Greeter', () => {
-	expect(Car).toBeDefined();
+import { GroupsIO } from '../src/index';
+test('Test Init', () => {
+	expect(GroupsIO).toBeDefined();
+	const groupIO = new GroupsIO();
+	expect(groupIO.IsLoggedIn()).toBeFalsy();
 });
